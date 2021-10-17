8ba8e1986bfbceca47f26b5dd1dea533cf76e2eb
[ganesh chuadhari] <[ganraaj1777@gmail.com]>
2021-10-17 16:47:01 +0530

    final commit final comm

--- StringCalculatorTDD/src/main/java/com/demo/beans/StringCalculator.java
+++ StringCalculatorTDD/src/main/java/com/demo/beans/StringCalculator.java
@@ -1,26 +1,87 @@
 package com.demo.beans;
 
-import java.util.List;
-
 public class StringCalculator {
 	 
-	public int Add(String number) {
-		if(number.isEmpty()) {
+	public int add(String number) {
+		if(number == "") {
 			return 0;
-		}else if(number.contains(",")) {
-			String[] splt = number.split(",");
-			int sum = 0;
-				for(String str:splt) {
-					sum += toInt(str);
-				}
-			return sum;
 		}
-		else {
-			return Integer.parseInt(number);
-		}
+		
+		number = removalOfDelimiters(number);
+		checkForNegativeNumbers(number);
+		number = removeLargeNumbers(number);
+		return sum(splitString(number));
 	}
 
-	private int toInt(String str) {
+	private int sum(String[] numbers) {
+		int sum = 0;
+
+		for(String num : numbers) {
+			sum += toInt(num);
+		}
+		return sum;
+	}
+
+	private static String[] splitString(String number) {
+		String[] numbers = number.split(",");
+		return number.split(",");
+	}
+
+	private static String removalOfDelimiters(String number) {
+		number = number.replace("\n", ",");
+
+		if(number.startsWith("//[")) {			
+			String delim = number.substring(3, number.indexOf("]"));
+			
+			if(delim.contains("-"))
+				throw new IllegalArgumentException("Illegal delimiter: " + delim);
+			
+			number = number.substring(5 + delim.length());
+			number = number.replace(delim, ",");
+		}
+
+		if (number.startsWith("//")) {
+			String delim = number.substring(2, 3);
+			
+			if(delim.contains("-"))
+				throw new IllegalArgumentException("Illegal delimiter: " + delim);
+			
+			number = number.substring(4);
+			number = number.replace(delim, ",");
+		}
+		return number;
+	}
+
+	private static int toInt(String str) {
 		return Integer.parseInt(str);
+	}
+	
+	private void checkForNegativeNumbers(String number) {
+		String[] negs = splitString(number);
+		String negatives = "";
+
+		for(String nums : negs) {
+			if(nums.contains("-")) {
+				negatives += nums + ",";
+			}
+		}
+
+		if(!negatives.isEmpty()) {
+			throw new IllegalArgumentException("Negatives not allowed: " + negatives);
+		}
+	}
+	
+	private static String removeLargeNumbers(String number) {
+		StringBuilder fixedString = new StringBuilder();
+
+		String[] nums = splitString(number);
+
+		for(String num : nums) {
+			if(toInt(num) <= 1000) {
+				fixedString.append(num + ",");
+			}
+		}
+
+		return fixedString.toString();
 	}
 }
--- StringCalculatorTDD/src/test/java/com/demo/test/TestStringCal.java
+++ StringCalculatorTDD/src/test/java/com/demo/test/TestStringCal.java
@@ -1,5 +1,6 @@
 package com.demo.test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -16,26 +17,120 @@ public class TestStringCal {
 	
 	@Test
 	public void testForZero() {
-		assertEquals(0,sc.Add(""));
+		assertEquals(0,sc.add(""));
 	}
 	
 	@Test
 	public void testForOneNumber() {
-		assertEquals(1,sc.Add("1"));
+		assertEquals(1,sc.add("1"));
 	}
 	
 	@Test
 	public void testForTwoNumbers() {
-		assertEquals(3,sc.Add("1,2"));
+		assertEquals(3,sc.add("1,2"));
 	}
 	
 	@Test
 	public void testForMultipleNumbers() {
-		assertEquals(6,sc.Add("1,2,3"));
+		assertEquals(6,sc.add("1,2,3"));
 	}
 	
 	@Test
 	public void testForMultipleNumbers2() {
-		assertEquals(10,sc.Add("1,2,3,4"));
+		assertEquals(10,sc.add("1,2,3,4"));
 	}
+	
+	@Test
+	public void testForDelimiter() {
+		assertEquals(6,sc.add("1,2\n3"));
+	}
+	
+	@Test
+	public void testForDelimiter2() {
+		assertEquals(11,sc.add("2\n3,4\n2"));
+	}
+	
+	@Test
+	public void testForNewDelimiter() {
+		assertEquals(3,sc.add("//;\n1;2"));
+	}
+	
+	@Test
+	public void testNegatives() {
+		Boolean threwException = false;
+        try {
+            sc.add("-4,-3");
+        }
+        catch(IllegalArgumentException ex) {
+            threwException = true;
+           System.out.print("\nException message for testNegativeTwoNumbers: " + ex.getMessage() + "\n");
+        }
+        assertTrue(threwException);
+	}
+	
+	
+	@Test
+    public void testNegativeNumbersWithDelim() {
+        Boolean threwException = false;
+        try {
+            sc.add("//;\n1;-2");
+        }
+        catch(IllegalArgumentException ex) {
+            threwException = true;
+            System.out.print("\nException message for testNegativeNumbersWithCustomDelim: " + ex.getMessage() + "\n");
+        }
+        assertTrue(threwException);
+    }
+	
+	@Test
+    public void testNegSignAsDelimiter() {
+        Boolean threwException = false;
+        try {
+            sc.add("//-\n5-3");
+        }
+        catch(IllegalArgumentException ex) {
+            threwException = true;
+            System.out.print("\nException message for testNegSignAsDelimiter: " + ex.getMessage() + "\n");
+        }
+        assertTrue(threwException);
+    }
+	
+	@Test
+    public void testLargeNumber() {
+        assertEquals(2, sc.add("2,1001"));
+    }
+	
+	@Test
+    public void test1000() {
+        assertEquals(1002, sc.add("1000,2"));
+    }
+	
+	@Test
+    public void testLargeNumberWithCustomDelimiter() {
+        assertEquals(3, sc.add("//;\n1001;3"));
+    }
+	
+	@Test
+    public void testLargeNumberWithNegativeNumbers() {
+        Boolean threwException = false;
+        try {
+            sc.add("5000,-3, 5");
+        }
+        catch(IllegalArgumentException ex) {
+            threwException = true;
+            System.out.print("\nException message for testNegSignAsDelimiter: " + ex.getMessage() + "\n");
+        }
+        assertTrue(threwException);
+    }
+	
+	@Test
+	public void testLengthyDelimiters() {
+		assertEquals(6, sc.add("//[***]\n1***2***3"));
+		assertEquals(6, sc.add("//[,**;*]\n1,**;*2,**;*3"));
+	 }
+	
+	@Test
+    public void testLengthyDelimiterWith1Symbol() {
+        assertEquals(6, sc.add("//[*]\n1*2*3"));
+    }
 }
